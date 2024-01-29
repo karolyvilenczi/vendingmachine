f4197866dbc66b27f4e8a4329211449b1ccb7821
Karesz <karolyvilenczi@github.com>
2024-01-29 10:05:50 +0100

    added some edge cases to productName and amountAvailable

--- app/busapp/services/inventory_manager.py
+++ app/busapp/services/inventory_manager.py
@@ -29,6 +29,31 @@ class Product(BaseModel):
 
         return value
 
+    @field_validator('productName')
+    @classmethod
+    def productName_must_be_a_non_empty_string_or_more_than_128_chars(cls, productName:str) -> str:
+        if len(productName) == 0:
+            raise ValueError("productName must not be an empty string.")
+    
+        if len(productName) >= 128:
+            raise ValueError("productName must not be longer than 128 characters.")
+
+        return productName
+    
+
+    @field_validator('amountAvailable')
+    @classmethod
+    def amountAvailable_cannot_be_negative(cls, amountAvailable:int) -> int:
+        if amountAvailable < 0:
+            raise ValueError("amountAvailable cannot be negative.")
+    
+        machine_capacity_for_product_amount = 100 # TODO: put this to a config
+        if amountAvailable > machine_capacity_for_product_amount:
+            raise ValueError(f"amountAvailable cannot be more than the machine capacity of {machine_capacity_for_product_amount}")
+
+        return amountAvailable
+
+
       
 
 class Inventory:
--- app/tests/busapp/services/test_inventory_manager.py
+++ app/tests/busapp/services/test_inventory_manager.py
@@ -20,6 +20,7 @@ async def test_product_object_gets_created():
 
     assert test_coke.__dict__ == test_code_dict, "OK"
 
+# tests for the cost field
 @pytest.mark.asyncio
 @pytest.mark.xfail(reason = "Trying to create a product with cost not a multiple of 5 should fail ")
 async def test_product_cost_must_be_a_multiple_of_5():    
@@ -35,3 +36,34 @@ async def test_product_cost_must_be_greater_than_0():
 @pytest.mark.xfail(reason = "Trying to create a product with cost more than 100000 should fail.")
 async def test_product_cost_must_be_less_than_10000():    
     test_coke = im.Product(id = 1, productName="Coke", amountAvailable=10, cost=1000000)
+
+# tests for the productName field
+@pytest.mark.asyncio
+@pytest.mark.xfail(reason = "Trying to create a product with empty string should fail.")
+async def test_product_name_cannot_be_an_empty_string():
+    test_coke = im.Product(id = 1, productName="", amountAvailable=10, cost=1000000)
+
+
+@pytest.mark.asyncio
+@pytest.mark.xfail(reason = "Trying to create a product with a name longer than 128 chars should fail.")
+async def test_product_name_with_string_longer_than_128_chars():
+
+    long_pepsi_name = "pepsi" * 32  # 4 characters in "pepsi", so 32 * 4 = 128
+    test_name = long_pepsi_name + "i"
+    test_coke = im.Product(id = 1, productName=test_name, amountAvailable=10, cost=10)
+
+# tests for the amountAvailable field
+@pytest.mark.asyncio
+@pytest.mark.xfail(reason = "Trying to create a product with amountAvailable less than 0 should fail.")
+async def test_product_amount_cannot_be_negative():
+    test_coke = im.Product(id = 1, productName="", amountAvailable=-1, cost=100)
+
+@pytest.mark.asyncio
+@pytest.mark.xfail(reason = "Trying to create a product with amountAvailable more than the machine capacity.")
+async def test_product_amount_cannot_be_more_than_machine_capacity():
+    machine_capacity_for_product_amount = 100 # TODO: move to config
+    test_coke = im.Product(id = 1, productName="", amountAvailable=machine_capacity_for_product_amount+1, cost=100)
+
+
+    
+
